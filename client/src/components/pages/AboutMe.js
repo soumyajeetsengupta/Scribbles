a406d905e146f6620c96be192f681299be2e8f84
Soumyajeet Sengupta <soumyajeetsengupta@gmail.com>
2024-01-02 17:12:09 +0530

    Added pending My Skill and What Can I do for you section contents adding their JS part is left for interactivity.

--- client/src/components/pages/AboutMe.js
+++ client/src/components/pages/AboutMe.js
@@ -5,18 +5,20 @@ export default function AboutMe () {
         <div id="main-about-me">
             <div className="post-Ex-wrapper contact-me-wrapper">
                 <div className="title">
-                    <h3>ABOUT IBLOGGO</h3>
+                    <h3>ABOUT SCRIBBLE</h3>
                 </div>
             </div>
 
             <div className="about-me-text post-Ex-wrapper contact-me-wrapper">
                 <img src="https://media.licdn.com/dms/image/D4D03AQG4GvTFemdxvg/profile-displayphoto-shrink_400_400/0/1701826717178?e=1709164800&v=beta&t=5TeEJY1M83h-KVDqZrfrIfOlyDGArnDoLkVawGYx5vE" alt="author-pfp" />
-                <p>Hi all. This is Ang and Annie from Istanbul. We're providing content for iBloggo site and we’ve been in internet, social media and affiliate for too long time and its my profession. We are web designer & developer living Amsterdam! What can I say, we are the best..</p>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+                <p>Scribble is not a polished manuscript, but a vibrant tapestry woven with messy threads of learning, life's surprises, and a deep-seated belief in sharing. It's my self-taught SDE journey unveiled, where code snippets dance with personal reflections, celebrating the beautiful imperfection of growth and connection.</p>
+                <p>Under the hood of Scribble hums the MERN stack, a potent cocktail of technologies blending the power of MongoDB for data, the magic of Express for server-side orchestration, the artistry of React for crafting captivating interfaces, and the unifying elegance of Node.js. It's a full-fledged playground for my developer journey, where lines of code dance with vibrant experiences, weaving a tapestry of learning and connection that's yours to explore. Welcome to Scribble, where MERN meets messy but magnificent exploration.</p>
                 <blockquote>
                     <i>I didn't have any special training or anything. I just had a computer and a lot of enthusiasm.</i>
                 </blockquote>
-                <p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                <p>Hi all! I'm Soumyajeet from India. I'm 23 years old.
+Self-taught fullstack dev by day, cybersecurity enthusiast by night. Building secure web experiences & exploring the fascinating world of digital defense.
+Join me on my coding (and hacking) journey!</p>
                 <div className="author-footer">
                     <div className="auth-foo-left">
                         
@@ -61,12 +63,79 @@ export default function AboutMe () {
             <div className='my-skill-section'>
                 <div className='my-skill post-Ex-wrapper p-p-c-wrapper'>
                     <div className="title">
-                        <h3>OUR SKILLS</h3>
+                        <h3>MY SKILLS</h3>
+                    </div>
+                    <div className='my-skill-rating-contents'>
+                        <div className='skill-1 skills-wrapper'>
+                            <p className='skill-name'>Javascript</p>
+                            <div className='skill-bar'>
+                                <div className='skill-bar-left'>
+                                    <div className='skill-measure'></div>
+                                </div>
+                                <div className='skill-bar-right'>80%</div>
+                            </div>
+                        </div>
+                        <div className='skill-2 skills-wrapper'>
+                            <p className='skill-name'>HTML5, CSS3, SCSS</p>
+                            <div className='skill-bar'>
+                                <div className='skill-bar-left'>
+                                    <div className='skill-measure'></div>
+                                </div>
+                                <div className='skill-bar-right'>80%</div>
+                            </div>
+                        </div>
+                        <div className='skill-3 skills-wrapper'>
+                            <p className='skill-name'>MongoDB</p>
+                            <div className='skill-bar'>
+                                <div className='skill-bar-left'>
+                                    <div className='skill-measure'></div>
+                                </div>
+                                <div className='skill-bar-right'>60%</div>
+                            </div>
+                        </div>
+                        <div className='skill-4 skills-wrapper'>
+                            <p className='skill-name'>Redux</p>
+                            <div className='skill-bar'>
+                                <div className='skill-bar-left'>
+                                    <div className='skill-measure'></div>
+                                </div>
+                                <div className='skill-bar-right'>60%</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='what-can-do post-Ex-wrapper p-p-c-wrapper'>
                     <div className="title">
                         <h3>WHAT CAN I DO FOR YOU?</h3>
+                    </div>
+                    <div className='accordian-container'>
+                        <div className='accordian-group click-on'>
+                            <div className='accordian-heading'>
+                                <div className='heading-symbol'><i class="fa-solid fm-sm fa-plus"></i></div>
+                                <div className='heading-text'>Let's craft beautiful & responsive interfaces.</div>
+                            </div>
+                            <div className='according-description desc-on'>
+                                <p>Need a sleek website that engages users? I'll spin up stunning front-end experiences with modern frameworks like React</p>
+                            </div>
+                        </div>
+                        <div className='accordian-group click-on'>
+                            <div className='accordian-heading'>
+                                <div className='heading-symbol'><i class="fa-solid fm-sm fa-plus"></i></div>
+                                <div className='heading-text'>Powerhouse your app with robust backend logic.</div>
+                            </div>
+                            <div className='according-description desc-on'>
+                                <p>Server-side sorcery needed? I'll wield Node.js to build secure and scalable backends that handle your data like a charm.</p>
+                            </div>
+                        </div>
+                        <div className='accordian-group click-on'>
+                            <div className='accordian-heading'>
+                                <div className='heading-symbol'><i class="fa-solid fm-sm fa-plus"></i></div>
+                                <div className='heading-text'>Data wrangling? I got this.</div>
+                            </div>
+                            <div className='according-description desc-on'>
+                                <p>Struggling to wrangle your data? I'll design and implement efficient databases like MySQL or MongoDB, keeping your information organized and accessible.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
