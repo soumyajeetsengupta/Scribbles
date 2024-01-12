5f8dce864bb1f8b0149173d51c5833b6a10cc3fe
soumyajeetsengupta <soumyajeetsengupta@gmail.com>
2024-01-12 22:28:27 +0530

    ui tweaks

--- client/src/components/pages/ExpandedPost.js
+++ client/src/components/pages/ExpandedPost.js
@@ -133,6 +133,48 @@ export default function ExpandedPost () {
                 </div>
             </div>
 
+            <div className='comment-section-mobile post-Ex-wrapper p-p-c-wrapper'>
+                <div className="title">
+                    <h2><span>3</span> Comments on The One-Man Maestro: How Chris Sawyer Crafted Gaming History</h2>
+                </div>
+
+                <div class='comment-output-screen-sm'>
+                    <div class="cmt">
+                      <div class="img-container">
+                        <img src='https://media.licdn.com/dms/image/D4D03AQF3Cu7CNTIkow/profile-displayphoto-shrink_800_800/0/1690113666633?e=1709769600&v=beta&t=8lgMQgvxPhBy9wadsRWaxmC4eWouEtDDbryTViG4fnw' alt='cmt-pfp-img' />
+                      </div>                   
+                        <div class='cmt-sm-text'>
+                            <span class='cmt-name-sm'>Surya </span>
+                            It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+                        </div>
+                    </div>
+                </div>
+
+                <div class='comment-output-screen-sm'>
+                    <div class="cmt">
+                         <div class="img-container">
+                         <img src='https://media.licdn.com/dms/image/D4D03AQGMHaAgysYYcA/profile-displayphoto-shrink_400_400/0/1700294784629?e=1709769600&v=beta&t=5bcLAtqYGPceLGyZAOhY4xYwRINbE1FBOo73-z99-yE' alt='cmt-pfp-img' />
+                        </div>                   
+                        <div class='cmt-sm-text'>
+                            <span class='cmt-name-sm'>Kasi</span>
+                            It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+                        </div>
+                    </div>
+                </div>
+
+                <div class='comment-output-screen-sm'>
+                    <div class="cmt">
+                        <div class="img-container">
+                        <img src='https://media.licdn.com/dms/image/C5603AQE5WcuHVq_E0w/profile-displayphoto-shrink_400_400/0/1636992757460?e=1709769600&v=beta&t=_iQapFGfOcL25ckCzrM0zIDMxhiIHdDeOpWnxNxZyAA' alt='cmt-pfp-img' />
+                        </div>                   
+                        <div class='cmt-sm-text'>
+                            <span class='cmt-name-sm'>Anurag</span>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="cmt-form post-Ex-wrapper p-p-c-wrapper">
                 <div className="title">
                     <h3>COMMENT FORM</h3>
@@ -143,9 +185,6 @@ export default function ExpandedPost () {
                     </div>
                     <div className='cmt-email-box'>
                         <input type='email' id='cmt-email' placeholder='Email Address' />
-                    </div>
-                    <div className='cmt-phone-box'>
-                        <input type='phone' id='cmt-number' placeholder='Phone Number' />
                     </div>
                     <div className='cmt-message-box'>
                         <textarea id='cmt-message' placeholder='Message'></textarea>
